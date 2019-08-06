2d8e7703daecb2b94a26901623e36ee6fb1bab07
Dustin heaps <dustinheaps89@gmail.com>
2019-08-06 18:35:05 -0400

    finished dashboard styling

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [MatInputModule, MatFormFieldModule],
-  exports: [MatInputModule, MatFormFieldModule]
+  imports: [MatInputModule, MatFormFieldModule, MatCardModule, MatIconModule, MatButtonModule],
+  exports: [MatInputModule, MatFormFieldModule, MatCardModule, MatIconModule, MatButtonModule]
 })
 export class MaterialModule {}
