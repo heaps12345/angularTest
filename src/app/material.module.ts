e6c6de1b8492d5e4f4df26c31b370e76efe88f25
Dustin heaps <dustinheaps89@gmail.com>
2019-08-07 18:35:24 -0400

    finished styled side nav

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,9 +4,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
-  imports: [MatInputModule, MatFormFieldModule, MatCardModule, MatIconModule, MatButtonModule],
-  exports: [MatInputModule, MatFormFieldModule, MatCardModule, MatIconModule, MatButtonModule]
+  imports: [MatInputModule, MatFormFieldModule, MatCardModule, MatIconModule, MatButtonModule, MatSidenavModule],
+  exports: [MatInputModule, MatFormFieldModule, MatCardModule, MatIconModule, MatButtonModule, MatSidenavModule]
 })
 export class MaterialModule {}
